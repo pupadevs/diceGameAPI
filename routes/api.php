f1a42eb0d58ff3db85eb344bfb01bdbbbfd2e1c7
Peterson A. Sena <petersonsena24@gmail.com>
2023-09-13 19:14:18 +0200

    add api pacth

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,30 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+//Login and register
+Route::post('login', [UserController::class, 'login'])->name('user.login');
+Route::post('players',[UserController::class,'register'])->name('user.register');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function(){
+    //Player
+
+    Route::get('/players',[UserController::class,'index'])->middleware('role:admin')->name('players.index'); //Player list
+
+    Route::patch('/players/{id}',[UserController::class, 'update'])->middleware('role:player')->name('playres.update'); //Update name
+
+    Route::get('player/ranking',[UserController::class,'allPlayerRate'])->middleware('role:admin')->name('ranking.index');//Rank all player
+
+    Route::get('/players/ranking/winner',[])->middleware('role:admin')->name('rankin.winner'); //Highest succes rate
+
+    Route::get('/players/ranking/loser',[])->middleware('role:admin')->name('ranking.loser'); //lowest succes rate
+
+    //Game
+
+    Route::get('/players/{id}/games',[GameController::class, 'index'])->middleware('role:player')->name('games.index'); // Games list
+
+    Route::post('/players/{id}/games',[GameController::class, 'throwDice'])->middleware('role:player')->name('games.throw'); //play game
+
+    Route::delete('/players/íd}/games',[GameController::class, 'destroy'])->middleware('role:player')->name('games.destroy'); //game destroy
+
+    Route::post('logout',[UserController::class, 'logout'])->middleware('role:admin|player')->name('user.logout'); //logout
 });
