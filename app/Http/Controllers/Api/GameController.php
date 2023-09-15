8326b2a461b9977e0e271a9b1794f49ebd645eed
Peterson A. Sena <petersonsena24@gmail.com>
2023-09-15 22:17:27 +0200

    function gamelogic, throwDice

--- app/Http/Controllers/Api/GameController.php
+++ app/Http/Controllers/Api/GameController.php
@@ -3,20 +3,20 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Game;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-
-
 
 use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
     //
-    private function gameLogic(){
+    private function gameLogic()
+    {
 
-        $dice1 = rand(1,6);
-        $dice2 = rand(1,6);
+        $dice1 = rand(1, 6);
+        $dice2 = rand(1, 6);
 
         return ([
             'dice1' => $dice1,
@@ -24,5 +24,24 @@ class GameController extends Controller
             'win' => $dice1 + $dice2 === 7
         ]);
     }
-   
+    public function throwDice($id)
+    {
+        $authId = Auth::id();
+        if ($authId == $id) {
+            $user = User::find($id);
+            $game = $user->games()->create($this->gameLogic());
+            return response()->json(
+                [
+                    'name' => $user->name,
+                    'dice1' => $game->dice1,
+                    'dice2' => $game->dice2,
+                    'win' => $game->win ? 'Win' : 'Lose',
+                    'message' => $game->win === true ? 'Haz ganado' : 'Mas suerte para la proxima'
+                ],
+                200
+            );
+        }
+        return response()->json(['message' => 'Unathorized user'], 401);
+    }
+    
 }
